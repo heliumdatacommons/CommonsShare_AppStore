6a3419683600e4e2636bcafecc67b6806fbede23
hyi <hongyi@renci.org>
2019-01-10 16:46:58 -0500

    some cleanup and needed addition for local dev env

--- CSv1/CSv1/urls.py
+++ CSv1/CSv1/urls.py
@@ -21,16 +21,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^accounts/login/$', views.signin_view, name='signin-view'),
     url(r'^signout_view$', views.signout_view),
     url(r'^accounts/signup/$', views.signup_view),
+    url(r'^accounts/signin/$', views.signin_view, name='signin-view'),
     url(r'^apps/$', views.show_apps, name='apps-view'),
     url(r'^$', views.home_page_view, name='home-page-view'),
-    #url(r'^phenotype/?husername=${HS_USR_NAME}&token=${HS_USR_TOKEN}&resourcetype=${HS_RES_TYPE}&resourceid=${HS_RES_ID}$',views.phenotype_analyze, name="phenotype_analyze"),
     url(r'^phenotype$', views.phenotype_analyze, name="phenotype_analyze"),
-    #url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    #url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    #url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    #url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
