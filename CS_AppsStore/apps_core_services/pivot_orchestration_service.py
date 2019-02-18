f9311428996302bf4fca58c96327b22e475d3531
hyi <hongyi@renci.org>
2019-02-18 16:38:02 -0500

    code optimization

---
+++ CS_AppsStore/apps_core_services/pivot_orchestration_service.py
@@ -0,0 +1,184 @@
+"""
+This service contains generic service functions that pertain to PIVOT deployment of appliances of
+various types, such as HAIL, i2b2_Transmart, Jupyter etc. The purpose of this service is to abstract
+requirement
+"""
+
+import re
+from json import dumps
+import requests
+import time
+from importlib import import_module
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_409_CONFLICT, HTTP_200_OK, \
+    HTTP_201_CREATED
+
+from django.utils import timezone
+from django.conf import settings
+from django.core.exceptions import ValidationError
+
+
+class PIVOTResourceException(Exception):
+    def __init__(self, message, code=None, params=None):
+        super(PIVOTResourceException, self).__init__(self, message, code, params)
+        self.message = message
+        self.code = code
+        self.params = params
+
+
+class PIVOTException(Exception):
+    def __init__(self, message, code=None, params=None):
+        super(PIVOTException, self).__init__(self, message, code, params)
+        self.message = message
+        self.code = code
+        self.params = params
+
+
+def validate_id(id_in):
+    """
+    validate appliance or container id to make sure it only contain dash, underline, letters,
+    and numbers required by pivot
+    :param id_in: appliance id or container id to be validated
+    :return: raise ValidationError upon failure
+    """
+    id_validation_failure_msg = 'appliance or container id {} failed validation - id can only ' \
+                                'contain dash, underline, letters, and numbers'
+    id_re = re.compile('^[a-zA-Z0-9_-]+$')
+    if not id_re.match(id_in):
+        # failure
+        raise ValidationError(id_validation_failure_msg.format(id_in))
+    # success
+    return
+
+
+def deploy_appliance(config_data, check_res_avail=False):
+    """
+    deploy appliance via PIVOT API
+    :param config_data: appliance configuration data dict to send to PIVOT
+    :param check_res_avail: optional with default being False indicating whether to check
+    resource availability before requesting PIVOT to deploy the appliance
+    :return: (True, redirect_url) upon success or raise PIVOTResourceException or PIVOTException
+    upon failure
+    """
+    # check if appliance already exists, if yes, do redirect directly, if not, create a new one
+    url = settings.PIVOT_URL + 'appliance'
+    app_id = config_data['id']
+    app_url = url + '/' + app_id
+    get_response = requests.get(app_url)
+    if get_response.status_code == HTTP_404_NOT_FOUND:
+        if check_res_avail:
+            # check whether there are enough resources available to create requested appliance
+            response = requests.get(settings.PIVOT_URL + 'cluster')
+            return_data = response.json()
+            avail_cpus = 0
+            avail_mems = 0
+            for cluster in return_data:
+                avail_cpus += cluster['resources']['cpus']
+                avail_mems += cluster['resources']['mem']
+
+            if avail_cpus < 1 or avail_mems < 1000:
+                raise PIVOTResourceException('There are no resource available at the moment')
+
+            # get requested cpus and mems
+            for con in config_data['containers']:
+                if con['id'] == 'workers':
+                    num_insts = con['instances']
+                    num_cpus = con['resources']['cpus']
+                    mem_size = con['resources']['mem']
+
+                    req_cpus = settings.INITIAL_COST_CPU + int(num_cpus) * int(num_insts)
+                    req_mem = settings.INITIAL_COST_MEM + int(mem_size) * int(num_insts)
+
+                    if req_cpus > avail_cpus or req_mem > avail_mems:
+                        # there are not enough resources, notify users to reduce number of
+                        # requested resources
+                        err_msg = 'There are not enough resources available. Please reduce ' \
+                                  'number of instances and resources requested to be within ' \
+                                  'our available resource pool ({} CPUs and {}MB memory ' \
+                                  'available at the moment).'
+                        a_cpus = int(avail_cpus)
+                        a_mems = int(avail_mems)
+                        raise PIVOTResourceException(err_msg.format(a_cpus, a_mems))
+
+                    break
+
+        # request to create the appliance
+        response = requests.post(url, data=dumps(config_data))
+        if response.status_code == HTTP_409_CONFLICT:
+            time.sleep(2)
+            response = requests.post(url, data=dumps(config_data))
+
+        if response.status_code != HTTP_200_OK and \
+                        response.status_code != HTTP_201_CREATED:
+            raise PIVOTException(response.text)
+
+    # success, return redirct_url
+    redirect_url = app_url + '/ui'
+    return redirect_url
+
+
+def get_running_appliances_usage_status(status_module_path, status_module_class, request_url=''):
+    """
+    return usage status of all running appliances by querying the status module class input.
+    The class from the imported module must have the following fields:
+    user, appliance_id, status, start_timestamp, end_timestamp. Additionally, the status_model
+    could include optional memory, cpus, insts fields which represent resources allocated for
+    the appliance, e.g., HAIL appliance
+    :param status_module_path: the path to be used to import the module
+    :param status_module_class: the class name to load from the module
+    :param request_url: optional parameter to check whether appliance has been deleted or not
+    :return: context dictionary upon success and raise ImportError or ValidationError upon failure
+    """
+    try:
+        status_module = import_module(status_module_path)
+    except ImportError as ex:
+        raise ImportError('module cannot be imported: ' + ex.message)
+
+    status_model = getattr(status_module, status_module_class)
+    fields = []
+    for field in status_model._meta.fields:
+        fields.append(field.name)
+
+    if 'user' not in fields or 'appliance_id' not in fields or 'status' not in fields or \
+            'start_timestamp' not in fields or 'end_timestamp' not in fields:
+        raise ValidationError('status model does not have required fields user, appliance_id, '
+                              'status, start_timestamp, or end_timestamp')
+
+    context = {'usage_list': []}
+
+    for obj in status_model.objects.filter(status='R'):
+        # check whether the appliance is really running since the appliance could be deleted
+        # in PIVOT
+        if request_url:
+            url = request_url + obj.appliance_id
+            get_response = requests.get(url)
+            if get_response.status_code == HTTP_404_NOT_FOUND:
+                # appliance has been deleted
+                obj.staus = 'D'
+                obj.end_timestamp = timezone.now()
+                obj.save()
+            else:
+                # appliance is live and being used, put it into context to send to caller
+                context['usage_list'].append({
+                    'username': obj.user.username,
+                    'user_fullname': obj.user.get_full_name(),
+                    'user_email': obj.user.email,
+                    'appliance_id': obj.appliance_id,
+                    'start_timestamp': obj.start_timestamp,
+                    'insts': obj.insts if 'insts' in fields else None,
+                    'memory': obj.memory if 'memory' in fields else None,
+                    'cpus': obj.cpus if 'cpus' in fields else None,
+                })
+        else:
+            # no need to check on whether appliance is live, just assume it is live
+            context['usage_list'].append({
+                'username': obj.user.username,
+                'user_fullname': obj.user.get_full_name(),
+                'user_email': obj.user.email,
+                'appliance_id': obj.appliance_id,
+                'start_timestamp': obj.start_timestamp,
+                'insts': obj.insts if 'insts' in fields else None,
+                'memory': obj.memory if 'memory' in fields else None,
+                'cpus': obj.cpus if 'cpus' in fields else None,
+            })
+
+    return context
--- CS_AppsStore/pivot_hail/views.py
+++ CS_AppsStore/pivot_hail/views.py
@@ -2,10 +2,6 @@
 from __future__ import unicode_literals
 
 import hashlib
-import re
-from json import load, dumps
-import requests
-import time
 
 from django.contrib import messages
 from django.shortcuts import render
@@ -13,13 +9,16 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.http import HttpResponseRedirect, JsonResponse, HttpResponseServerError
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
-from rest_framework.status import HTTP_409_CONFLICT, HTTP_404_NOT_FOUND, HTTP_200_OK, \
-    HTTP_201_CREATED, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_409_CONFLICT, HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR, \
+    HTTP_400_BAD_REQUEST
 
 from pivot_hail.models import HailConfig, HailStatus
+from pivot_hail.utils import get_hail_start_context
 from apps_core_services.utils import check_authorization
-from apps_core_services.orchestrator_service import get_running_appliances_usage_status
+from apps_core_services.pivot_orchestration_service import validate_id, \
+    get_running_appliances_usage_status, deploy_appliance, PIVOTException, PIVOTResourceException
 
 
 # Create your views here.
@@ -42,11 +41,10 @@ def deploy(request):
     conf_data['id'] = app_id
 
     # validate app_id
-    id_validation_failure_msg = 'appliance or container id {} failed validation - id can only ' \
-                                'contain dash, underline, letters, and numbers'
-    id_re = re.compile('^[a-zA-Z0-9_-]+$')
-    if not id_re.match(app_id):
-        return JsonResponse(data={'error': id_validation_failure_msg.format(app_id)},
+    try:
+        validate_id(app_id)
+    except ValidationError as ex:
+        return JsonResponse(data={'error': ex.message},
                             status=HTTP_500_INTERNAL_SERVER_ERROR)
 
     num_insts = request.POST.get('num_instances', None)
@@ -59,9 +57,12 @@ def deploy(request):
         cluster_name = ''
 
     for con in conf_data['containers']:
-        if con['id'] and not id_re.match(con['id']):
-            return JsonResponse(data={'error': id_validation_failure_msg.format(con['id'])},
-                                status=HTTP_400_BAD_REQUEST)
+        if con['id']:
+            try:
+                validate_id(con['id'])
+            except ValidationError as ex:
+                return JsonResponse(data={'error': ex.message},
+                                    status=HTTP_500_INTERNAL_SERVER_ERROR)
 
         if cluster_name:
             con['rack'] = cluster_name
@@ -76,150 +77,64 @@ def deploy(request):
             if token:
                 con['env']['JUPYTER_TOKEN'] = token
 
-    # check if appliance already exists, if yes, do redirect directly, if not, create a new one
-    url = settings.PIVOT_URL + 'appliance'
-    app_url = url + '/' + app_id
-    get_response = requests.get(app_url)
-    if get_response.status_code == HTTP_404_NOT_FOUND:
-        # check whether there are enough resources available to create requested HAIL cluster
-        response = requests.get(settings.PIVOT_URL + 'cluster')
-        return_data = response.json()
-        avail_cpus = 0
-        avail_mems = 0
-        for cluster in return_data:
-            avail_cpus += cluster['resources']['cpus']
-            avail_mems += cluster['resources']['mem']
+    try:
+        redirect_url = deploy_appliance(conf_data, check_res_avail=True)
+        # new appliance has been created successfully, record new entry into hail cluster
+        # status table and make sure status of the existing running appliance get updated from
+        # running to deleted
+        curr_time = timezone.now()
+        HailStatus.objects.filter(user=user, appliance_id=app_id, status='R'). \
+            update(status='D', end_timestamp=curr_time)
+        HailStatus.objects.create(user=user, appliance_id=app_id, status='R',
+                                  insts=num_insts, memory=mem_size, cpus=num_cpus,
+                                  start_timestamp=curr_time)
+        return JsonResponse(status=HTTP_200_OK, data={'url': redirect_url})
 
-        if avail_cpus < 1 or avail_mems < 1000:
-            return JsonResponse(data={'error': 'There are no resource available at the moment.'
-                                               'Please check <a href="/pivot_hail/status/">HAIL '
-                                               'cluster current usage status</a> to see who are '
-                                               'currently using HAIL cluster.'},
-                                status=HTTP_400_BAD_REQUEST)
+    except PIVOTResourceException as ex:
+        err_msg = ex.message + 'Please check <a href="/pivot_hail/status/">HAIL cluster current ' \
+                              'usage status</a> to see who are currently using HAIL cluster.'
+        return JsonResponse(data={'error': err_msg},
+                            status=HTTP_400_BAD_REQUEST)
 
-        req_cpus = settings.INITIAL_COST_CPU + int(num_cpus) * int(num_insts)
-        req_mem = settings.INITIAL_COST_MEM + int(mem_size) * int(num_insts)
-
-
-        if req_cpus <= avail_cpus and req_mem <= avail_mems:
-            # there are enough resources, go ahead to request to create HAIL cluster
-            response = requests.post(url, data=dumps(conf_data))
-            if response.status_code ==HTTP_409_CONFLICT:
-                time.sleep(2)
-                response = requests.post(url, data=dumps(conf_data))
-
-            if response.status_code != HTTP_200_OK and \
-                            response.status_code != HTTP_201_CREATED:
-                return JsonResponse(data={'error': response.text},
-                                    status=HTTP_500_INTERNAL_SERVER_ERROR)
-            else:
-                # new appliance has been created successfully, record new entry into hail cluster
-                # status table
-                # make sure status of the existing running appliance get updated from running to
-                # deleted
-                curr_time = timezone.now()
-                HailStatus.objects.filter(user=user, appliance_id=app_id, status='R').\
-                    update(status='D', end_timestamp=curr_time)
-                HailStatus.objects.create(user=user, appliance_id=app_id, status='R',
-                                          insts=num_insts, memory=mem_size, cpus=num_cpus,
-                                          start_timestamp=curr_time)
-        else:
-            err_msg = 'There are not enough resources available. Please reduce number of ' \
-                      'instances and resources requested to be within our available resource pool ' \
-                      '({} CPUs and {}MB memory available at the moment). Check ' \
-                      '<a href="/pivot_hail/status/">HAIL cluster current usage status</a> to see who ' \
-                      'are currently using HAIL cluster.'
-            # there are not enough resources, notify users to reduce number of requested resources
-            a_cpus = int(avail_cpus)
-            a_mems = int(avail_mems)
-            return JsonResponse(data={'error': err_msg.format(a_cpus, a_mems)},
-                                status=HTTP_400_BAD_REQUEST)
-
-    redirect_url = app_url + '/ui'
-    return JsonResponse(status=HTTP_200_OK, data={'url': redirect_url})
+    except PIVOTException as ex:
+        return JsonResponse(data={'error': ex.message},
+                            status=HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @login_required
 def login_start(request):
-    conf_qs = HailConfig.objects.all()
-    if not conf_qs:
-        config_file = 'pivot_hail/data/pivot_hail.json'
-        with open(config_file, 'r') as fp:
-            p_data = load(fp)
-            if not 'id' in p_data or not 'containers' in p_data:
-                errmsg = "PIVOT JSON Configuration file is not valid"
-                messages.error(request, errmsg)
-                if request.is_ajax():
-                    return JsonResponse(data={'error': errmsg},
-                                        status=HTTP_500_INTERNAL_SERVER_ERROR)
-                else:
-                    return HttpResponseRedirect(request.META['HTTP_REFERER'])
-            HailConfig.objects.create(data=p_data)
-    else:
-        p_data = HailConfig.objects.all().first().data
+    # view function when the start action is triggered from CommonsShare Apps Store from
+    # which the user has already logged in to CommonsShare Apps Store directly
+    context = get_hail_start_context(request)
+    if 'error' in context:
+        messages.error(request, context['error'])
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
     # get default value to show on the start page for users to override as needed
-    context = {}
-    for con in p_data['containers']:
-        if con['id'] == 'workers':
-            insts = con['instances']
-            cpus = con['resources']['cpus']
-            mems = con['resources']['mem']
-            context = {
-                'num_instances': str(insts),
-                'num_cpus': str(cpus),
-                'mem_size': str(mems)
-            }
-            break
-
     return render(request, "pivot_hail/start.html", context)
 
 
 def start(request):
+    # view function when the start action is triggered from CommonsShare
     auth_resp = check_authorization(request)
     if auth_resp.status_code != 200:
         return HttpResponseRedirect("/")
     else:
-        conf_qs = HailConfig.objects.all()
-        if not conf_qs:
-            config_file = 'pivot_hail/data/pivot_hail.json'
-            with open(config_file, 'r') as fp:
-                p_data = load(fp)
-                if not 'id' in p_data or not 'containers' in p_data:
-                    errmsg = "PIVOT JSON Configuration file is not valid"
-                    messages.error(request, errmsg)
-                    if request.is_ajax():
-                        return JsonResponse(data={'error': errmsg},
-                                            status=HTTP_500_INTERNAL_SERVER_ERROR)
-                    else:
-                        return HttpResponseRedirect(request.META['HTTP_REFERER'])
-                HailConfig.objects.create(data=p_data)
-        else:
-            p_data = HailConfig.objects.all().first().data
+        context = get_hail_start_context(request)
+        if 'error' in context:
+            messages.error(request, context['error'])
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
         # get default value to show on the start page for users to override as needed
-        context = {}
-        for con in p_data['containers']:
-            if con['id'] == 'workers':
-                insts = con['instances']
-                cpus = con['resources']['cpus']
-                mems = con['resources']['mem']
-                context = {
-                    'num_instances': str(insts),
-                    'num_cpus': str(cpus),
-                    'mem_size': str(mems)
-                }
-                break
-
         return render(request, "pivot_hail/start.html", context)
 
 
 @login_required()
 def status(request):
     url = settings.PIVOT_URL + 'appliance/'
-    context, err_msg = get_running_appliances_usage_status('pivot_hail.models', 'HailStatus',
-                                                           request_url=url)
-    if err_msg:
-        return HttpResponseServerError(err_msg)
-    else:
+    try:
+        context = get_running_appliances_usage_status('pivot_hail.models', 'HailStatus',
+                                                      request_url=url)
         return render(request, "pivot_hail/status.html", context)
+    except (ImportError, ValidationError) as ex:
+        return HttpResponseServerError(ex.message)
