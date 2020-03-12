ea73f163584af61d8d7e624e9016b9b229af1286
Chuck Bennett <cnbennett3@gmail.com>
2020-03-12 11:18:00 -0400

    #322 Integrate user registration email with OIDC work.

--- CS_AppsStore/CS_AppsStore/settings.py
+++ CS_AppsStore/CS_AppsStore/settings.py
@@ -60,6 +60,24 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+# Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'bot.commonsshare@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'bot.commonsshare@gmail.com'
+DEFAULT_SUPPORT_EMAIL = 'bot.commonsshare@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+SOCIALACCOUNT_QUERY_EMAIL = ACCOUNT_EMAIL_REQUIRED
+
+
 AUTHENTICATION_BACKENDS = (
     'apps_core_services.backends.oauth.OAuth',
     'django.contrib.auth.backends.ModelBackend',
